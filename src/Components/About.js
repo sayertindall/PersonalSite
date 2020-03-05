9d46371d9152d600a0b4cfd9f3fa1d5e7dea2745
Sayer Tindall <sayertindall@Sayers-MacBook-Pro.local>
2020-03-04 18:03:46 -0800

    remove alt text

--- src/Components/About.js
+++ src/Components/About.js
@@ -22,7 +22,7 @@ class About extends Component {
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
+            <img className="profile-pic"  src={profilepic} alt="" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
